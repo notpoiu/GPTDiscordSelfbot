cad4f4dbcbd54eef584229e60d29c01468840284
poiu <75510171+notpoiu@users.noreply.github.com>
2023-09-08 18:44:37 -0400

    Update installreqs.bat
    pluh

--- installreqs.bat
+++ installreqs.bat
@@ -3,3 +3,4 @@ title requirement installer
 py -3 -m pip install git+https://github.com/dolfies/discord.py-self@renamed#egg=selfcord.py[voice]
 pip install openai
 echo requirements were downloaded successfully!
+pause
